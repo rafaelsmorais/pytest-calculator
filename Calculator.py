d82a9ec6a9201e67a6961db0f1210b87f8c22b6e
Rafael Morais <contato@rafaelmoraisjr.com>
2025-02-21 22:36:08 -0300

    criação da class Calculator

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def somar(x,y):
+        return x + y;
+    
+    def subtrair(x,y):
+        return x - y;
+
+    def multiplicar(x,y):
+        return x * y;
+
+    def dividir(x,y):
+        return x / y;
+        
